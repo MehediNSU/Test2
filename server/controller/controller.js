c84c4dbdea53975848e413a29590aa1db70943a3
MehediNSU <mehedi.hasan26@northsouth.edu>
2022-10-10 14:37:42 +0600

    complete create API

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model');
+
+//Create and save new user
+export.create = (req, res) =>{
+
+}
+
+//Retrive and return all users/retrive and retuen a single user
+export.find = (req, res) =>{
+    
+}
+
+//Update a new identified user by user id
+export.update = (req, res) =>{
+    
+}
+
+//Delete a user with specify user id in the request
+export.dleete = (req, res) =>{
+    
+}
