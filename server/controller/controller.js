aaf0db53621cac57f36efb4d5c563c26626265a0
MehediNSU <mehedi.hasan26@northsouth.edu>
2022-10-10 15:07:03 +0600

    complete Create

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,21 +1,46 @@
 var Userdb = require('../model/model');
 
 //Create and save new user
-export.create = (req, res) =>{
+exports.create = (req, res) =>{
+//validate request
+if(!req.body){
+    res.status(400).send({message:"Content can not be empty!"});
+    return;
+ }
+
+//new user
+const user = new Userdb({
+    name:req.body.name,
+    email:req.body.email,
+    gender:req.body.gender,
+    status:req.body.status
+})
+
+//save user in the database
+user
+.save(user)
+.then(data=>{
+    res.send(data)
+})
+.catch(err => {
+    res.status(500).send({
+        message: err.message || "Some error occured creating a create operation"
+    });
+});
 
 }
 
 //Retrive and return all users/retrive and retuen a single user
-export.find = (req, res) =>{
+exports.find = (req, res) =>{
     
 }
 
 //Update a new identified user by user id
-export.update = (req, res) =>{
+exports.update = (req, res) =>{
     
 }
 
 //Delete a user with specify user id in the request
-export.dleete = (req, res) =>{
+exports.delete = (req, res) =>{
     
 }
