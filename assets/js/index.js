a9e029c2f30c3037bfa04b90b27d71cc3ae4eac0
MehediNSU <mehedi.hasan26@northsouth.edu>
2022-10-10 19:52:57 +0600

    complete Delete User

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,6 +20,26 @@ $("#update_user").submit(function(event){
         "data": data
     }
     $.ajax(request).done(function(response){
-        alert('Data Updated Succesfully');
+        alert('Data Updated Succesfully!');
     })
 })
+
+if(window.location.pathname == "/"){
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function(){
+        var id = $(this).attr("data-id")
+
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`,
+            "method":"DELETE",
+        }
+
+        if(confirm ("Do you really want to delete this record?")){    
+            $.ajax(request).done(function(response){
+                alert('Data Dedated Succesfully!');
+                location.reload();
+            })      
+        }
+
+    })
+}
